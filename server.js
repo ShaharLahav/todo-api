4308c235d502c40a2e8d73f9a4c3ea8a70890fd9
Shahar Lahav <lahav.shahar@gmail.com>
2016-01-31 12:38:47 +0200

    refactoring underscore

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet dad for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Learn NodeJs',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/' , function (req, res) {
 	res.send('Todo API Root');
@@ -26,19 +19,33 @@ app.get('/todos' , function (req , res) {
 
 app.get('/todos/:id' , function (req , res) {
 	var todoID = parseInt(req.params.id , 10);
-	var index;
-	for (var i = 0; i < todos.length; i++) 
-	{
-		if(todos[i].id === todoID)
-			index = i;
-	}
-	if(typeof index === 'undefined')
+	var matched = _.findWhere(todos, {id: todoID});
+	if(typeof matched === 'undefined')
 		res.status(404).send();
 	else
 	{
-		res.json(todos[index]);
+		res.json(matched);
 	}
 });
+
+app.post('/todos' , function (req , res) {
+	var body = _.pick(req.body, 'description' , 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)
+	{
+		return res.status(400).send();
+	}
+	
+
+	body.description = body.description.trim();
+
+	var addToDo = body;
+	addToDo.id = todoNextId++;
+	todos.push(addToDo);
+	console.log('description: ' + body.description);
+
+	res.json(body);
+})
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT);
